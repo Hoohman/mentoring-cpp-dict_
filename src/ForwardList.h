426d4b9d52f68f2abdf4a8fa97993c3199a955aa
Hoohman <->
2025-01-14 05:05:19 +0300

    added Node for ForwardList

--- src/ForwardList.h
+++ src/ForwardList.h
@@ -1,3 +1,29 @@
-template <typename T> class ForwardList {
-  // ...
+#ifndef FORWARDLIST
+#define FORWARDLIST
+
+template <typename T>
+class ForwardList
+{
+public:
+    /*ForwardList();
+    ~ForwardList();
+
+    void push_front(T value);
+    void pop_front();
+    void clear();*/
+
+private:
+    class Node
+    {
+        T data_;
+        Node* next_;
+
+        Node(T value);
+    };
+
+private:
+    Node* head_;
+    size_t size_;
 };
+
+#endif // FORWARDLIST
